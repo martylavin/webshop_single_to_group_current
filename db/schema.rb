ec645c762b60fb6fa02fe5f9f7229ee1a8b8495d
mlavin <martylavin1@yahoo.com>
2016-03-24 21:57:27 -0500

    change sendmail pass and put in message for db field  and email

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160105170355) do
+ActiveRecord::Schema.define(version: 20160312223524) do
 
   create_table "admins", force: true do |t|
     t.string   "first_name"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20160105170355) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "admin_id"
+    t.text     "message"
   end
 
 end
