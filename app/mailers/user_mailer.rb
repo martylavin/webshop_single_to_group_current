37a28249265f66af0c0cba65d77f972102f1a234
mlavin <martylavin1@yahoo.com>
2016-07-01 11:57:52 -0500

    added july and aug newsletter

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,22 +1,23 @@
 class UserMailer < ActionMailer::Base
-  default from: "from@example_Martylavin.com"
+  default from: "Martylavin1@gmail.com"
 
   def contactus(user)
     @user = user
-    #@greeting = "Hi"
-    @greeting = "American Legion Post 854 Carries On"
+    @greeting = "Hi"
+    #@greeting = "American Legion Post 854 Carries On"
     #attachments['rails.png'] = File.read("app/assets/images/rails.png")
     #attachments['rails.2016_JanFebNL.pdf'] = File.read("app/assets/images/2016_JanFebNL.pdf")
     #attachments['rails.2016_MAYJUNE_v1.pdf'] = File.read("app/assets/images/2016_MAYJUNE_v1.pdf")
     #attachments['Steve_Davies.pdf'] = File.read("app/assets/images/Steve_Davies.pdf")
+    attachments['JULYAUGUST2016v1.pdf'] = File.read("app/assets/images/JULYAUGUST2016v1.pdf")
     mail(to: user.email, subject: "The Legion Post 854 Email for #{user.name } ")
   
     
   end
 
   def newsletter(users)
-    #@greeting = "Hi"
-    @greeting = "American Legion Post 854 Carries On"
+    @greeting = "Hi"
+    #@greeting = "American Legion Post 854 Carries On"
     @users = users
   
     mail_array = [] 
@@ -28,8 +29,9 @@ class UserMailer < ActionMailer::Base
     #attachments['rails.png'] = File.read("app/assets/images/rails.png")
     #attachments['rails.2016_JanFebNL.pdf'] = File.read("app/assets/images/2016_JanFebNL.pdf")
     #attachments['rails.2016_MAYJUNE_v1.pdf'] = File.read("app/assets/images/2016_MAYJUNE_v1.pdf")
-    #attachments['Steve_Davies.pdf'] = File.read("app/assets/images/Steve_Davies.pdf")
-    mail(to: mail_array_string_final, subject: "The Legion Post 854 Email for the new Movie Clip")
+    #attachments['Steve_Davies.pdf'] = File.read("app/assets/images/Steve_Davies.pdf") JULYAUGUST2016v1.pdf
+    attachments['JULYAUGUST2016v1.pdf'] = File.read("app/assets/images/JULYAUGUST2016v1.pdf")
+    mail(to: mail_array_string_final, subject: "The Legion Post 854 Email Newsletter")
    
   end
 
