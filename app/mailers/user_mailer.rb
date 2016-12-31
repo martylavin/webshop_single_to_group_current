05ca5f13d1d48a6728bb65ca067b29f475f070ed
mlavin <martylavin1@yahoo.com>
2016-12-30 19:43:09 -0600

    added 2017 janfeb pdf to mail

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -10,14 +10,14 @@ class UserMailer < ActionMailer::Base
     #attachments['rails.2016_MAYJUNE_v1.pdf'] = File.read("app/assets/images/2016_MAYJUNE_v1.pdf")
     #attachments['Steve_Davies.pdf'] = File.read("app/assets/images/Steve_Davies.pdf")
     #attachments['JULYAUGUST2016v1.pdf'] = File.read("app/assets/images/2016_JULYAUGUST2016v1.pdf")
-    attachments['2016_SeptOct_v1.pdf'] = File.read("app/assets/images/2016_SeptOct_v1.pdf")
+    attachments['2017_jan_feb_newsletter.pdf'] = File.read("app/assets/images/2017_jan_feb_newsletter.pdf")
     mail(to: user.email, subject: "The Legion Post 854 Email for #{user.name } ")
   
     
   end
 
   def newsletter(users)
-    @greeting = "OOPS, THIS IS THE CORRECT ONE, 2016 NOVEMBER AND DECEMBER NEWSLETTER"
+    @greeting = "2017 January and February Newsletter"
     #@greeting = "American Legion Post 854 Carries On"
     @users = users
   
@@ -32,7 +32,7 @@ class UserMailer < ActionMailer::Base
     #attachments['rails.2016_MAYJUNE_v1.pdf'] = File.read("app/assets/images/2016_MAYJUNE_v1.pdf")
     #attachments['Steve_Davies.pdf'] = File.read("app/assets/images/Steve_Davies.pdf") JULYAUGUST2016v1.pdf
     #attachments['JULYAUGUST2016v1.pdf'] = File.read("app/assets/images/2016_JULYAUGUST2016v1.pdf")
-    attachments['2016_NOVDEC_NEWSLETTER.pdf'] = File.read("app/assets/images/2016_NOVDEC_NEWSLETTER.pdf")
+    attachments['2017_jan_feb_newsletter.pdf'] = File.read("app/assets/images/2017_jan_feb_newsletter.pdf")
     mail(to: mail_array_string_final, subject: "The Legion Post 854 Email Newsletter")
    
   end
